972016e75b0beb44a36f7bed79f7439258422f97
Tim Bultmann <tbu@inmedias.it>
2018-10-02 16:37:00 +0200

    Add GPL v2

--- check_ahsay.py
+++ check_ahsay.py
@@ -1,5 +1,24 @@
 #!/usr/bin/env python3
 
+
+# check_ahsay
+# Copyright (C) 2018 inmedis.it GmbH
+#
+# This file is part of check_ahsay.
+#
+# check_ahsay is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+# check_ahsay is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with check_ahsay. If not, see <http://www.gnu.org/licenses/>.
+
 import argparse
 import requests
 import json
