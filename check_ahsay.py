2891d60f4a32502edb67a5ece8e303a096a71a7a
Jon Bulica <jonbulica99@gmail.com>
2020-01-20 14:17:24 +0100

    Fixes #1

--- check_ahsay.py
+++ check_ahsay.py
@@ -21,27 +21,26 @@
 import argparse
 import requests
 import json
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
 
-OK = ['BS_STOP_SUCCESS', 'BS_STOP_SUCCESS_WITH_ERROR', 'BS_STOP_SUCCESS_WITH_WARNING', 'OK']
+OK = ['BS_STOP_SUCCESS', 'BS_STOP_SUCCESS_WITH_ERROR',
+      'BS_STOP_SUCCESS_WITH_WARNING', 'OK']
 WARNING = ['BS_STOP_BY_USER']
-CRITICAL = ['BS_STOP_BY_SYSTEM_ERROR', 'BS_STOP_BY_SCHEDULER', 'BS_STOP_BY_QUOTA_EXCEEDED', 'BS_STOP_MISSED_BACKUP', 'BS_BACKUP_NOT_FINISHED', 'BS_MISSED_DATABASAE', 'BS_MISSED_PUBLIC_FOLDER', 'BS_MISSED_VIRTUAL_MACHINE', 'BS_STILL_RUNNING', 'BS_STOP_INCOMPLETE' ]
+CRITICAL = ['BS_STOP_BY_SYSTEM_ERROR', 'BS_STOP_BY_SCHEDULER', 'BS_STOP_BY_QUOTA_EXCEEDED', 'BS_STOP_MISSED_BACKUP', 'BS_BACKUP_NOT_FINISHED',
+            'BS_MISSED_DATABASAE', 'BS_MISSED_PUBLIC_FOLDER', 'BS_MISSED_VIRTUAL_MACHINE', 'BS_STILL_RUNNING', 'BS_STOP_INCOMPLETE']
 exit_code = 0
+exit_code_map = {
+    0: 'OK',
+    1: 'WARNING',
+    2: 'CRITICAL',
+    3: 'UNKNOWN'
+}
+
 
 def set_exit_code(code):
     global exit_code
     if code > exit_code:
         exit_code = code
-
-def get_status():
-    if exit_code == 0:
-        return 'OK'
-    elif exit_code == 1:
-        return 'WARNING'
-    elif exit_code == 2:
-        return 'CRITICAL'
-    else:
-        return 'UNKNOWN'
 
 
 def set_status(status):
@@ -54,9 +53,15 @@ def set_status(status):
     else:
         set_exit_code(3)
 
+
+def parse_date(date_str):
+    return datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+
+
 def main():
     # parse variables from commandline
-    parser = argparse.ArgumentParser(description='Nagios check for Ahsay backup states')
+    parser = argparse.ArgumentParser(
+        description='Nagios check for Ahsay backup states')
     parser.add_argument('--hostname',
                         '-H',
                         required=True,
@@ -106,7 +111,8 @@ def main():
     }
 
     try:
-        response = requests.post('http://' + args.hostname + '/obs/api/json/ListBackupJobStatus.do', data=payload)
+        response = requests.post(
+            'http://' + args.hostname + '/obs/api/json/ListBackupJobStatus.do', data=payload)
     except requests.ConnectionError:
         print("Could not connect to Ahsay API")
         exit(3)
@@ -115,20 +121,46 @@ def main():
         data = content.get('Data')
         output = ''
         if data:
+            # order backup jobs by date
+            data.sort(key=lambda x: parse_date(x['StartTime']))
+
+            backup_jobs = dict()
+            other_jobs = dict()
             # backups are available
             for i in data:
                 job_name = i.get('BackupSetName')
-                backup_status = i.get('BackupJobStatus')
+                # check for existing jobs
+                other_job = backup_jobs.get(job_name, None)
+                if other_job:
+                    other_jobs[job_name] = other_job
+                else:
+                    backup_jobs[job_name] = i
+
+            # run check logic on jobs we care about
+            for i, job in enumerate(backup_jobs.values()):
+                backup_status = job.get('BackupJobStatus')
                 set_status(backup_status)
-                output += 'Backup Job "{}" returned status {}\n'.format(job_name, backup_status)
+                output += '{index}. Backup Job "{name}" at {date} returned status {status}\n'.format(
+                    index=i + 1, name=job.get('BackupSetName'), date=job.get('StartTime'), status=backup_status)
+
+            # if we have superseded jobs, print them anyway
+            if other_jobs.values():
+                output += '\n--- Superseded jobs at {date} ------\n'.format(
+                    date=date_formatted)
+                for otherjob in other_jobs.values():
+                    output += '* Job "{name}" at {date} –– Status: {status}\n'.format(
+                        name=otherjob.get('BackupSetName'), date=otherjob.get('StartTime'), status=other_job.get('BackupJobStatus'))
+                output += 40*'-' + '\n'
         else:
             # there are no backups available
             set_status(content.get('Status'))
             output += content.get('Message')
 
-        output = '{}\n{}'.format(get_status(), output)
+        output = '{}\n{}'.format(
+            exit_code_map.get(exit_code, 'UNKNOWN'), output)
         print(output)
         exit(exit_code)
+
 
 if __name__ == '__main__':
     main()
